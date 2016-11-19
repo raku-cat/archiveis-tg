0d7b64ffbcafde4d0c2ca449ac687e031a464fd4
Raku-cat <raku@rakutiki.tv>
2016-11-19 00:24:07 -0600

    Finished refactor, added verbosity cause why the hell not

--- bot.py
+++ bot.py
@@ -19,6 +19,7 @@ def on_chat_command(msg):
 		return
 	command = msg['text'].lower()
 	if (command.split(' ')[0] == '/archive' or command.split(' ')[0] == '/archive@archiveisbot'):
+		print('Valid command from ' + chat_id)
 		try:
 			is_reply = msg['reply_to_message']
 		except KeyError:
@@ -27,71 +28,70 @@ def on_chat_command(msg):
 			command = is_reply['text']
 	else:
 		return
+	print('Sending archive link')
 	bot.sendMessage(chat_id, link_handler(command), reply_to_message_id=msg_id)
 
 def on_inline_query(msg):
 	query_id, form_id, query_string = telepot.glance(msg, flavor='inline_query')
-	print('before compute')
-	print(query_string)
-	archive_uri = link_handler(query_string)
-	print(archive_uri)
+#	print(query_string)
+	print('Query ' + query_id + ' recieved')
+	archive_uri =link_handler(query_string)
+	if archive_uri == None:
+		return
 	def compute():
-		print('compute start')
-#		archive_uri = link_handler(query_string)
-		print(archive_uri)
-		if archive_uri == None:
-			return
-		elif '.fo' in archive_uri:
+		if '.fo' in archive_uri:
 			archive_json = [InlineQueryResultArticle(
 					id='url', title=archive_uri,
 					input_message_content=InputTextMessageContent(
 						message_text=archive_uri),
 					)]
 		else:
-			print('start else')
-			timegate = 'https://archive.fo/timemap'
+			timegate = 'https://archive.fo/timemap/'
 			mc = MementoClient(timegate_uri=timegate, check_native_timegate=False)
 			timemap = mc.get_memento_info(query_string).get('timegate_uri')
 			hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',}
 			r = requests.get(timemap, headers=hdr)
 			archive_map = r.text
-			map_list = re.findall(b'\<(.*?)\>', archive_map)[2:-1]
-			map_decode = [x.decode('utf-8') for x in map_list]
-			print(archive_uri)
+			map_list = re.findall('\<(.*?)\>', archive_map)[2:-1]
 			archive_json = []
-			for x in map_decode:
-				rnint = str(random.randint(1,30000))
+			if len(map_list)> 50:
+				map_list = re.findall('\<(.*?)\>', archive_map)[2:50]
+			for x in map_list:
+				rnint = str(random.randint(1,3000))
 				archive_json.append(InlineQueryResultArticle(
 					id=rnint, title=x,
 				input_message_content=InputTextMessageContent(
 				message_text=x)
 			))
-				print(archive_json)
+#				print(archive_json)
 		return archive_json
+	print('Sending query response\n')
 	answerer.answer(msg, compute)
 
 def link_handler(link):
 	uri_rec = re.search("(?P<url>https?://[^\s]+)", link)
 	if uri_rec:
+		print('Url found')
 		uri = uri_rec.group("url")
-		print('dr breen')
-		print(uri)
+#		print(uri)
 		timegate = 'https://archive.fo/timegate/'
 		mc = MementoClient(timegate_uri=timegate, check_native_timegate=False)
 		try:
 			archive_uri = mc.get_memento_info(uri).get("mementos").get("last").get("uri")[0]
-			print(uri)
-			print(archive_uri)
+#			print(uri)
+#			print(archive_uri)
+			print('Archive is ' + archive_uri)
 		except:
 			url = 'https://archive.fo/submit/'
 			values = { 'url' : uri }
 			headers = { 'User-Agent' : 'Telegram archive bot - https://github.com/raku-cat/archiveis-tg' }
 			r = requests.post(url, data=values, headers=headers)
 			response = r.text
-			print(response)
+#			print(response)
 			archive_uri = response.split('"')[1]
+			print('Archive creation sucessful')
 		else:
-			return
+			return archive_uri
 	else:
 		return
 	return archive_uri
